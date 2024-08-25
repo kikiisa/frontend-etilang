b92f98641b8c46671ae1c9c346bf4a39f6c3ad06
kikiisa <kikiisa89@gmail.com>
2024-08-25 17:34:09 +0800

    fix

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,10 +1,2 @@
-const api_service = [
-    {
-        service_python:"https://modern-grouper-moderately.ngrok-free.app/"
-    },
-    {
-        service_admin:"https://sparrow-popular-basilisk.ngrok-free.app/"
-    }
-]
-export const service1 = api_service[0].service_python;        
-export const service2 = api_service[0].service_admin;
+export const service1 = "https://modern-grouper-moderately.ngrok-free.app/api/";
+export const service2 = "https://sparrow-popular-basilisk.ngrok-free.app/api/";
