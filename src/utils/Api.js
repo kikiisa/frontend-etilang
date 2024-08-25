c51f05fdbef348e87e0fc05109cb3baebc5a04d4
kikiisa <kikiisa89@gmail.com>
2024-08-25 17:27:30 +0800

    connect to api

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,2 +1,10 @@
-export const service1 = "http://localhost:5000/api/";
-export const service2 = "http://localhost:8000/api/";
+const api_service = [
+    {
+        service_python:"https://modern-grouper-moderately.ngrok-free.app/"
+    },
+    {
+        service_admin:"https://sparrow-popular-basilisk.ngrok-free.app/"
+    }
+]
+export const service1 = api_service[0].service_python;        
+export const service2 = api_service[0].service_admin;
